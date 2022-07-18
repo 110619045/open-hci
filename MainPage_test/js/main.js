7863b3535e28921bd9e1116033ff0e67dc74eab0
110619045 <43201134+110619045@users.noreply.github.com>
2022-07-18 12:13:50 +0800

    Update main.js

--- MainPage_test/js/main.js
+++ MainPage_test/js/main.js
@@ -1,7 +1,3 @@
-if ( iPhone|iPad|iPod/i.test(navigator.platform)) {
-  window.location.href= "https://blog.xuite.net/tolarku/blog/66314217";
-}
-
 import { main } from "./tower.js"
 
 $(function() {
